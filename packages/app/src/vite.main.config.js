97796afa4ffc16c092bf7aa5cc9da7510859582c
slime7 <slime7@foxmail.com>
2025-04-10 15:35:04 +0800

    更新依赖

--- packages/app/src/vite.main.config.js
+++ packages/app/src/vite.main.config.js
@@ -1,60 +1,61 @@
 import { join } from 'node:path';
 import { builtinModules } from 'node:module';
 import { external } from '../package.json';
+import { defineConfig } from 'vite';
 
 const PACKAGE_ROOT = join(__dirname, 'main');
 
-/**
- * @type {import('vite').UserConfig}
+/**=
  * @see https://vitejs.dev/config/
  */
-const config = {
-  mode: process.env.MODE,
-  root: PACKAGE_ROOT,
-  envDir: process.cwd(),
-  resolve: {
-    alias: [
-      {
-        find: /^@pkg\/(.*)/,
-        replacement: `${join(PACKAGE_ROOT, '..')}/$1`,
-      },
-      {
-        find: /^@\/(.*)/,
-        replacement: `${join(PACKAGE_ROOT, '../renderer')}/$1`,
-      },
-    ],
-  },
-  build: {
-    sourcemap: process.env.NODE_ENV === 'development' ? 'inline' : false,
-    target: 'node14',
-    outDir: join(PACKAGE_ROOT, '../../dist/main'),
-    assetsDir: '.',
-    minify: process.env.MODE === 'development' ? false : 'terser',
-    terserOptions: process.env.MODE === 'development' ? undefined : {
-      ecma: 2020,
-      compress: {
-        passes: 2,
-      },
-      safari10: false,
-    },
-    lib: {
-      entry: 'index.js',
-      formats: ['cjs'],
-    },
-    rollupOptions: {
-      external: [
-        'electron',
-        'electron-devtools-installer',
-        ...builtinModules,
-        ...external,
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+  return {
+    mode,
+    root: PACKAGE_ROOT,
+    envDir: process.cwd(),
+    resolve: {
+      alias: [
+        {
+          find: /^@pkg\/(.*)/,
+          replacement: `${join(PACKAGE_ROOT, '..')}/$1`,
+        },
+        {
+          find: /^@\/(.*)/,
+          replacement: `${join(PACKAGE_ROOT, '../renderer')}/$1`,
+        },
       ],
-      output: {
-        entryFileNames: '[name].js',
-      },
     },
-    emptyOutDir: true,
-    brotliSize: false,
-  },
-};
-
-export default config;
+    build: {
+      sourcemap: isDev ? 'inline' : false,
+      target: 'node14',
+      outDir: join(PACKAGE_ROOT, '../../dist/main'),
+      assetsDir: '.',
+      minify: isDev ? false : 'terser',
+      terserOptions: isDev ? undefined : {
+        ecma: 2020,
+        compress: {
+          passes: 2,
+        },
+        safari10: false,
+      },
+      lib: {
+        entry: 'index.js',
+        formats: ['cjs'],
+      },
+      rollupOptions: {
+        external: [
+          'electron',
+          'electron-devtools-installer',
+          ...builtinModules,
+          ...external,
+        ],
+        output: {
+          entryFileNames: '[name].js',
+        },
+      },
+      emptyOutDir: true,
+      brotliSize: false,
+    },
+  };
+});
--- packages/app/src/vite.renderer.config.js
+++ packages/app/src/vite.renderer.config.js
@@ -2,7 +2,7 @@
 
 import { join, resolve } from 'node:path';
 import { builtinModules } from 'node:module';
-import { normalizePath } from 'vite';
+import { defineConfig, normalizePath } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
@@ -12,73 +12,73 @@ const MODULES_ROOT = join(__dirname, '../node_modules');
 const isProd = process.env.MODE === 'production';
 
 /**
- * @type {import('vite').UserConfig}
  * @see https://vitejs.dev/config/
  */
-const config = {
-  mode: process.env.MODE,
-  root: RENDERER_ROOT,
-  resolve: {
-    alias: [
-      {
-        find: /^@pkg\/(.*)/,
-        replacement: `${join(RENDERER_ROOT, '..')}/$1`,
-      },
-      {
-        find: /^@\/(.*)/,
-        replacement: `${join(RENDERER_ROOT)}/$1`,
-      },
-      {
-        find: /^vue$/,
-        replacement: isProd ? 'app://./libs/vue/vue.esm-browser.js' : 'http://localhost:5173/libs/vue/vue.esm-browser.js',
-      },
-    ],
-  },
-  plugins: [
-    /* 使插件和本体都能使用同一个 vue 实例，将 vue 在构建后放入根目录为两者提供引用 */
-    viteStaticCopy({
-      targets: [
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+  return {
+    mode,
+    root: RENDERER_ROOT,
+    resolve: {
+      alias: [
         {
-          src: normalizePath(resolve(MODULES_ROOT, `./vue/dist/vue.esm-browser${isProd ? '.prod' : ''}.js`)),
-          dest: 'libs/vue',
-          rename: 'vue.esm-browser.js',
+          find: /^@pkg\/(.*)/,
+          replacement: `${join(RENDERER_ROOT, '..')}/$1`,
+        },
+        {
+          find: /^@\/(.*)/,
+          replacement: `${join(RENDERER_ROOT)}/$1`,
+        },
+        {
+          find: /^vue$/,
+          replacement: isProd ? 'app://./libs/vue/vue.esm-browser.js' : 'http://localhost:5173/libs/vue/vue.esm-browser.js',
         },
       ],
-    }),
-    vue(),
-    /* 使用自定义 sass 变量导致 dev 启动出问题
-    vuetify({
-      styles: {
-        configFile: 'plugins/variables.scss',
-      },
-    }),
-    */
-    vuetify(),
-  ],
-  base: '',
-  server: {
-    fs: {
-      strict: true,
     },
-  },
-  build: {
-    outDir: join(RENDERER_ROOT, '../../dist/renderer'),
-    emptyOutDir: true,
-    minify: false,
-    commonjsOptions: {},
-    sourcemap: process.env.NODE_ENV === 'development' ? 'inline' : false,
-    target: 'modules',
-    rollupOptions: {
-      input: {
-        main: join(RENDERER_ROOT, 'index.html'),
-        pluginIndex: join(RENDERER_ROOT, 'plugin-index.html'),
+    plugins: [
+      /* 使插件和本体都能使用同一个 vue 实例，将 vue 在构建后放入根目录为两者提供引用 */
+      viteStaticCopy({
+        targets: [
+          {
+            src: normalizePath(resolve(MODULES_ROOT, `./vue/dist/vue.esm-browser${isProd ? '.prod' : ''}.js`)),
+            dest: 'libs/vue',
+            rename: 'vue.esm-browser.js',
+          },
+        ],
+      }),
+      vue(),
+      /* 使用自定义 sass 变量导致 dev 启动出问题
+       vuetify({
+       styles: {
+       configFile: 'plugins/variables.scss',
+       },
+       }),
+       */
+      vuetify(),
+    ],
+    base: '',
+    server: {
+      fs: {
+        strict: true,
       },
-      external: [
-        'vue',
-        ...builtinModules,
-      ],
     },
-  },
-};
-
-export default config;
+    build: {
+      outDir: join(RENDERER_ROOT, '../../dist/renderer'),
+      emptyOutDir: true,
+      minify: false,
+      commonjsOptions: {},
+      sourcemap: isDev ? 'inline' : false,
+      target: 'modules',
+      rollupOptions: {
+        input: {
+          main: join(RENDERER_ROOT, 'index.html'),
+          pluginIndex: join(RENDERER_ROOT, 'plugin-index.html'),
+        },
+        external: [
+          'vue',
+          ...builtinModules,
+        ],
+      },
+    },
+  };
+});
